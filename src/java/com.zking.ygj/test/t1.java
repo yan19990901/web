025e5eb405aee8d5671b186409a5386c99c038f0
yan19990901 <afdyt12345@qq.com>
2019-12-02 13:40:00 +0800

    你好，桂均

--- src/java/com.zking.ygj/test/t1.java
+++ src/java/com.zking.ygj/test/t1.java
@@ -1,8 +1,7 @@
 package com.zking.ygj.test;
 
-public class webTest {
-
+public class t1 {
     public static void main(String[] args) {
-        System.out.println("你好桂均++++");
+        System.out.println("你好");
     }
 }
