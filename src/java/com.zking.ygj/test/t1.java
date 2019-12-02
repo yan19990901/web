4a9b5512bd6c31d7b410dc21fab28ad069668cf7
yan19990901 <afdyt12345@qq.com>
2019-12-02 13:57:20 +0800

    abcd

--- src/java/com.zking.ygj/test/t1.java
+++
@@ -1,8 +0,0 @@
-package com.zking.ygj.test;
-
-public class t1 {
-    public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("hello");
-    }
-}
--- src/java/com.zking.ygj/test/webTest.java
+++
@@ -1,8 +0,0 @@
-package com.zking.ygj.test;
-
-public class webTest {
-
-    public static void main(String[] args) {
-        System.out.println("你好桂均++++");
-    }
-}
